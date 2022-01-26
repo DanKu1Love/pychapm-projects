d1cf40f0e41a4bae0a31f143c420a5421dc0f47b
Danila Kudryashov <DanKu1Love@yandex.ru>
2022-01-26 17:33:55 +0300

    'initial'

---
+++ lesson3.py
@@ -0,0 +1,59 @@
+def main_function():
+	action = input('\nВведите операцию: ')
+	count_of_actions = int(input('Введите кол-во операндов: '))
+	answer = 0
+	count_numbers = ''
+
+	if action == '+':
+		for plus in range(count_of_actions):
+			number = int(input('Введите число: '))
+			answer += number
+			if plus - count_of_actions == -1:
+				count_numbers += str(number) + ' ='
+			else:
+				count_numbers += str(number) + ' + '
+		print(count_numbers, answer)
+
+	
+	elif action == '-':
+		for plus in range(count_of_actions):
+			number = int(input('Введите число: '))
+			if plus == 0:
+				answer = number
+			else:
+				answer -= number
+			if plus - count_of_actions == -1:
+				count_numbers += str(number) + ' ='
+			else:
+				count_numbers += str(number) + ' - '
+		print(count_numbers, answer)
+
+	elif action == '*':
+		answer = 1
+		for plus in range(count_of_actions):
+			number = int(input('Введите число: '))
+			answer *= number
+			if plus - count_of_actions == -1:
+				count_numbers += str(number) + ' ='
+			else:
+				count_numbers += str(number) + ' * '
+		print(count_numbers, answer)
+
+	elif action == '/':
+		for plus in range(count_of_actions):
+			number = int(input('Введите число: '))
+			if plus == 0:
+				answer = number
+			else:
+				answer /= number
+			if plus - count_of_actions == -1:
+				count_numbers += str(number) + ' ='
+			else:
+				count_numbers += str(number) + ' / '
+		print(count_numbers, answer)
+
+	else:
+		print('Ошибка ввода. Попробуйте снова:')
+		main_function()
+
+main_function()
